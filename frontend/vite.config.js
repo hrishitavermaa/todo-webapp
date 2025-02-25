37c6a862b5419a574cf2d51edd98a19805ce5ff8
Hrishita Verma <e22cseu0939@bennett.edu.in>
2025-02-25 23:40:24 +0530

    fixed render.com routing issue 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
+    historyApiFallback: true, 
     proxy: {
       '/api': {
         target: 'https://todo-webapp-ya5m.onrender.com',
